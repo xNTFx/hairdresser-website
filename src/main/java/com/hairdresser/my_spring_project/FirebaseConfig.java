bc01c73bae001d176b329ad49d98ce41232ec59e
xNTFVx <135262384+xNTFx@users.noreply.github.com>
2024-10-17 17:49:35 +0200

    Update FirebaseConfig.java

--- src/main/java/com/hairdresser/my_spring_project/FirebaseConfig.java
+++ src/main/java/com/hairdresser/my_spring_project/FirebaseConfig.java
@@ -3,25 +3,37 @@ package com.hairdresser.my_spring_project;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${FIREBASE_PROJECT_ID}")
+    private String projectId;
+
+    @Value("${FIREBASE_PRIVATE_KEY}")
+    private String privateKey;
+
+    @Value("${FIREBASE_CLIENT_EMAIL}")
+    private String clientEmail;
+
     @PostConstruct
     public void initialize() {
         try {
-            FileInputStream serviceAccount = new FileInputStream("src/main/resources/hairdresser-site-isi-firebase-adminsdk-a7nix-c4006ff25b.json");
-
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(ServiceAccountCredentials.fromPkcs8(null, clientEmail, privateKey, null, Collections.emptyList()))
+                    .setProjectId(projectId)
                     .build();
 
             FirebaseApp.initializeApp(options);
+            System.out.println("Firebase initialized successfully.");
         } catch (IOException e) {
             e.printStackTrace();
         }
